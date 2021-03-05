63f00eeb38bc8905729e4f01ec0a1ff9520c417d
ryank2022 <ryankereszturi@gmail.com>
2021-03-05 11:18:04 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+ /*
+
+   }*/
+     function mouse_up(){
+
+ class Timer {
+  constructor () {
+    this.isRunning = false;
+    this.startTime = 0;
+    this.overallTime = 0;
+  }
+
+  _getTimeElapsedSinceLastStart () {
+    if (!this.startTime) {
+      return 0;
+    }
+  
+    return Date.now() - this.startTime;
+  }
+
+  start () {
+    if (this.isRunning) {
+      return console.error('Timer is already running');
+    }
+
+    this.isRunning = true;
+
+    this.startTime = Date.now();
+  }
+
+  stop () {
+    if (this.isRunning) {
+      return document.getElementById(time).textContent = 0;
+    }
+
+    this.isRunning = false;
+
+    this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
+  }
+
+  reset () {
+    this.overallTime = 0;
+    
+
+    if (this.isRunning) {
+      this.startTime = Date.now();
+      return;
+    }
+
+    this.startTime = 0;
+  }
+
+  getTime () {
+    if (!this.startTime) {
+      return 0;
+    }
+
+    if (this.isRunning) {
+      return this.overallTime + this._getTimeElapsedSinceLastStart();
+    }
+
+    return this.overallTime;
+  }
+}      var stopTime = [
+           this.isRunning = true,
+           this.startTime = 0,
+           this.overallTime = 0
+        
+]
+
+
+const timer = new Timer();
+timer.start();
+setInterval(() => {
+  const timeInSeconds = Math.round(timer.getTime() / 1000);
+  document.getElementById('time').innerText = timeInSeconds;
+  StopTime()
+
+     })}
+   
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
