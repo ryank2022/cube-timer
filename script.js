a748fa0cd9b34fa3a75a7c771a8a6d1e0a4f53b2
ryank2022 <ryankereszturi@gmail.com>
2021-03-08 22:34:00 -0700

    added a scrabling feature and fixed stop timer

--- script.js
+++ script.js
@@ -1,98 +1,104 @@
- /*
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
-   }*/
-     function mouse_up(){
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
 
- class Timer {
-  constructor () {
-    this.isRunning = false;
-    this.startTime = 0;
-    this.overallTime = 0;
-  }
+let interval = null;
 
-  _getTimeElapsedSinceLastStart () {
-    if (!this.startTime) {
-      return 0;
-    }
-  
-    return Date.now() - this.startTime;
-  }
+let status = "stopped";
 
-  start () {
-    if (this.isRunning) {
-      return console.error('Timer is already running');
-    }
+function stopWatch(){
 
-    this.isRunning = true;
+    seconds++;
 
-    this.startTime = Date.now();
-  }
+    if(seconds / 60 === 1){
+        seconds = 0;
+        minutes++;
 
-  stop () {
-    if (this.isRunning) {
-      return document.getElementById(time).textContent = 0;
+        if(minutes / 60 === 1){
+            minutes = 0;
+            hours++;
+        }
+
     }
 
-    this.isRunning = false;
-
-    this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
-  }
-
-  reset () {
-    this.overallTime = 0;
-    
-
-    if (this.isRunning) {
-      this.startTime = Date.now();
-      return;
+    if(seconds < 10){
+        displaySeconds = "0" + seconds.toString();
+    }
+    else{
+        displaySeconds = seconds;
     }
 
-    this.startTime = 0;
-  }
-
-  getTime () {
-    if (!this.startTime) {
-      return 0;
+    if(minutes < 10){
+        displayMinutes = "0" + minutes.toString();
+    }
+    else{
+        displayMinutes = minutes;
     }
 
-    if (this.isRunning) {
-      return this.overallTime + this._getTimeElapsedSinceLastStart();
+    if(hours < 10){
+        displayHours = "0" + hours.toString();
+    }
+    else{
+        displayHours = hours;
     }
 
-    return this.overallTime;
-  }
-}      var stopTime = [
-           this.isRunning = true,
-           this.startTime = 0,
-           this.overallTime = 0
-        
+    //Display updated time values to user
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+
+}
+
+
+
+function startStop(){
+
+    if(status === "stopped"){
+
+        //Start the stopwatch (by calling the setInterval() function)
+        interval = window.setInterval(stopWatch, 1000);
+        document.getElementById("startStop").innerHTML = "Stop";
+        status = "started";
+
+    }
+    else{
+
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML = "Start";
+        status = "stopped";
+
+    }
+
+}
+
+function reset(){
+
+    window.clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.getElementById("display").innerHTML = "00:00:00";
+    document.getElementById("startStop").innerHTML = "Start";
+
+}
+let scramble_cube = [
+   'r',
+   'r\'',
+   'l',
+   'l\'',
+   'b',
+   'b\'',
+   'd',
+   'd\'',   
+   'f',   
+   'f\''
 ]
-
-
-const timer = new Timer();
-timer.start();
-setInterval(() => {
-  const timeInSeconds = Math.round(timer.getTime() / 1000);
-  document.getElementById('time').innerText = timeInSeconds;
-  StopTime()
-
-     })}
-   
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let random_scramble = "";
+let i;
+for (i = 0; i < scramble_cube.length; i++) {
+   let num = Math.floor(Math.random()*9)
+   random_scramble += scramble_cube[num]
+}   
+document.getElementById("scramble").innerHTML = random_scramble;
